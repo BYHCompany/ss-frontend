c6a55fe1add3160159a3f6ad9ff5f44f5d153164
quintui <rscrafter1234@icloud.com>
2021-05-28 15:57:32 +0300

    App Container is ready!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './style.scss';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app_container"></div>
+    </div>
+  );
 }
 
 export default App;
