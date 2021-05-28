fa3680570e3c999cbaafb32e710941d18b0d5034
quintui <rscrafter1234@icloud.com>
2021-05-28 21:45:37 +0300

    NN4 Type Select is ready !

---
+++ src/Components/TypeSelect/TypeSelect/TypeSelect.tsx
@@ -0,0 +1,26 @@
+import { Button } from 'byh-components';
+import React from 'react';
+import { typeSelectButtons } from '../../../assets/typeSelectButtons';
+import './style.scss';
+
+export const TypeSelect = () => {
+  const [clicked, setClicked] = React.useState('');
+  return (
+    <div className="type-selector" data-testid="type-selector">
+      {typeSelectButtons.map((button) => (
+        <Button
+          onClick={() => setClicked(button.id)}
+          label={button.label}
+          key={button.id}
+          paddingHorizontal={5}
+          accurateValues
+          width={225}
+          height={40}
+          shadow
+          fontSize={24}
+          variant={button.id === clicked ? 'primary' : 'default'}
+        />
+      ))}
+    </div>
+  );
+};
