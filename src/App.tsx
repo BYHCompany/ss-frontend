a053ade9f82656cbc20e61faaee25e55bacb618b
Artem <artemsimutin@gmail.com>
2021-06-04 17:00:39 +0300

    NN15 Cookie Popup not finished until the end

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles/style.scss';
 import { BigAdvertPreview } from './Components/BigAdvertPreview';
 import { carExample } from './assets/bigAdvertPreview';
+import { CookiePopup } from './Components/CookiePopup/CookiePopup/CookiePopup';
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
       <div className="app theme-light">
         <Header />
         <div className="container">
-          <BigAdvertPreview
+          {/* <BigAdvertPreview
             car={carExample}
             src="https://i.ytimg.com/vi/Xa8DwaXVcS0/maxresdefault.jpg"
-          />
+          /> */}
+          <CookiePopup />
         </div>
         <Footer />
       </div>
