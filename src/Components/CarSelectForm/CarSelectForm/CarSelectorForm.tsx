6b166cdf3ced25827ba290f5e8face790c0e9744
Artem <artemsimutin@gmail.com>
2021-06-04 14:22:04 +0300

    NN15 started develop Cookie popup

--- src/Components/CarSelectForm/CarSelectForm/CarSelectorForm.tsx
+++ src/Components/CarSelectForm/CarSelectForm/CarSelectorForm.tsx
@@ -1,15 +1,15 @@
-import { Paper } from 'byh-components';
-import React from 'react';
-import { MakePicker } from '../Components/MakePicker/MakePicker';
-import { ModelForm } from '../Components/ModelForm/ModelForm';
+// import { Paper } from 'byh-components';
+// import React from 'react';
+// import { MakePicker } from '../Components/MakePicker/MakePicker';
+// import { ModelForm } from '../Components/ModelForm/ModelForm';
 
-export const CarSelectorForm = () => {
-  const [data, setData] = React.useState<carSelectType>({});
+// export const CarSelectorForm = () => {
+//   const [data, setData] = React.useState<carSelectType>({});
 
-  return (
-    <Paper style={{ padding: 15 }} height={397}>
-      <MakePicker setData={setData} />
-      <ModelForm setData={setData} />
-    </Paper>
-  );
-};
+//   return (
+//     <Paper style={{ padding: 15 }} height={397}>
+//       <MakePicker setData={setData} />
+//       <ModelForm setData={setData} />
+//     </Paper>
+//   );
+// };
---
+++ src/Components/CookiePopup/CookiePopup/CookiePopup.tsx
@@ -0,0 +1,11 @@
+import { Paper } from 'byh-components';
+import React from 'react';
+import { FaCookieBite } from 'react-icons/fa';
+
+export const CookiePopup = () => {
+  return (
+    <Paper className="cookiePopup__wrapper">
+      <FaCookieBite />
+    </Paper>
+  );
+};
