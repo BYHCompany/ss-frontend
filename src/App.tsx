7c12a9f5ea21205695634584634c134cec4091ce
Artem <trystatik@icloud.com>
2021-06-06 00:43:27 +0300

    deleted Paginator from frontend and relocated to lib

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles/style.scss';
+import { Paginator } from 'byh-components';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="app theme-light">
         <Header />
         <div className="container">
+          <Paginator allPagesCount={200} currentPage={66} />
         </div>
         <Footer />
       </div>
--- src/Components/Paginator/Paginator/PaginatorTypes.ts
+++
@@ -1,5 +0,0 @@
-export interface PaginatorProps {
-  allPagesCount: number;
-  currentPage: number;
-  callback?: (index: number) => void;
-}
