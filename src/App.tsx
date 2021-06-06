c06f207617fe812d57ec205ad689d139618b94bb
Quintui <rscrafter1234@icloud.com>
2021-06-06 14:52:40 +0300

    added specialOffers wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles/style.scss';
+import { MainPage } from './Pages/MainPage';
 
 function App() {
   return (
     <Router>
       <div className="app theme-light">
         <Header />
-        <div className="container"></div>
+        <div className="container">
+          <MainPage />
+        </div>
         <Footer />
       </div>
     </Router>
--- src/Pages/MainPage/MainPage/Components/SpecialOffers/SpecialOffers.tsx
+++ src/Pages/MainPage/MainPage/Components/SpecialOffers/SpecialOffers.tsx
@@ -1,24 +1,26 @@
 import { Button, Title } from 'byh-components';
 import React from 'react';
 import { advertData } from '../../../../../assets/advertPreviewData';
-
+import './SpecialOffers.scss';
 import { AdvertPreview } from '../../../../../Components/AdvertPreview';
 
 export const SpecialOffers = () => {
   return (
     <div>
       <Title text="Предложения специально для Вас" type="large" variant="primary" />
-      {advertData.map((props) => (
-        <AdvertPreview
-          id={props.id}
-          photo={props.photo}
-          price={props.price}
-          title={props.title}
-          year={props.year}
-          isElectric={props.isElectric}
-          isInTrending={props.isInTrending}
-        />
-      ))}
+      <div className="advert-preview__wrapper">
+        {advertData.map((props) => (
+          <AdvertPreview
+            id={props.id}
+            photo={props.photo}
+            price={props.price}
+            title={props.title}
+            year={props.year}
+            isElectric={props.isElectric}
+            isInTrending={props.isInTrending}
+          />
+        ))}
+      </div>
 
       <Button label="Показать больше" height={60} width={570} variant="primary" />
     </div>
