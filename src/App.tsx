95ef2f73778ff9b7ea85075e9b16591b0e010d2b
Artem <artemsimutin@gmail.com>
2021-06-06 14:52:46 +0300

    removed paginator from Paginator

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles/style.scss';
-import { Paginator } from 'byh-components';
 
 function App() {
   return (
     <Router>
       <div className="app theme-light">
         <Header />
-        <div className="container">
-          <Paginator allPagesCount={200} currentPage={66} />
-        </div>
+        <div className="container"></div>
         <Footer />
       </div>
     </Router>
