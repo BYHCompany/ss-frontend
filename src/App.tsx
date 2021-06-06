c2331efd13f873120cb04c7c7f911f8bae215902
Artem <artemsimutin@gmail.com>
2021-06-06 16:19:36 +0300

    no changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './GlobalStyles/style.scss';
-import { CookiePopup } from './Components/CookiePopup';
 
 function App() {
   return (
     <Router>
       <div className="app theme-light">
         <Header />
-        <div className="container">
-          <CookiePopup />
-        </div>
+        <div className="container"></div>
         <Footer />
       </div>
     </Router>
