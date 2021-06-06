7973a74d762730713596a6fbddf52fff271e9205
Artem <trystatik@icloud.com>
2021-06-06 21:14:33 +0300

    NN2 Fixed Footer responsive design

--- src/Components/Footer/Footer/Footer.tsx
+++ src/Components/Footer/Footer/Footer.tsx
@@ -10,14 +10,12 @@ export const Footer: React.FC = (): React.ReactElement => {
   return (
     <div className="footer">
       <div className="container footer__container">
-        <div className="footer__container__grid footer__container__top">
+        <div className="footer__container__grid">
           <List className="footer__container__grid__item" items={support} label="Поддержка" />
           <List className="footer__container__grid__item" items={info} label="Доп. информация" />
-          <div style={{ marginLeft: '200px', maxWidth: '170px' }}>
+          <div>
             <List items={contacts} label="Контакты" />
           </div>
-        </div>
-        <div className="footer__container__grid footer__container__bottom">
           <Payment className="footer__container__grid__item" icons={icons} label="Способы оплаты" />
           <Mobile className="footer__container__grid__item" label="Для мобильных устройств" />
         </div>
