88bbbcc137b590d16f181a0315f493c3f3ba0025
Quintui <rscrafter1234@icloud.com>
2021-06-16 13:47:23 +0300

    AdvertTitle into a seperate component

--- src/Pages/AdvertPage/AdvertPage/AdvertPage.tsx
+++ src/Pages/AdvertPage/AdvertPage/AdvertPage.tsx
@@ -2,6 +2,7 @@ import { Title } from 'byh-components';
 import React from 'react';
 import { AdvertInfo } from '../Components/AdvertInfo/AdvertInfo';
 import { AdvertSlider } from '../Components/AdvertSlider/AdvertSlider';
+import { AdvertTitle } from '../Components/AdvertTitle/AdvertTitle';
 import { CarEquipment } from '../Components/CarEquipment/CarEquipment';
 import { Comment } from '../Components/Comment/Comment';
 import './AdvertPage.scss';
@@ -9,9 +10,7 @@ export const AdvertPage = () => {
   return (
     <div className="advert-page__wrapper">
       <div className="advert-page__main-content">
-        <Title style={{ marginBottom: 20 }} variant="primary" type="small">
-          Porsche Taycan Turbo S
-        </Title>
+        <AdvertTitle />
         <AdvertSlider />
         <Comment />
         <CarEquipment />
